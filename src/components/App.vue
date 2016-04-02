33ae1824ffe772c65b18912996dae3e5529aaae6
Nicholas Law <nick@nicholaslaw.com.au>
2016-04-02 15:19:03 +1100

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			// note: changing this line won't causes changes
+			// with hot-reload because the reloaded component
+			// preserves its current state and we are modifying
+			// its initial state.
+		    msg: 'Hello Vue and Node Webkit!'
+		}
+	}
+}
+</script>
+
+<style scoped>
+h1 {
+	color: #f00;
+}
+</style>
