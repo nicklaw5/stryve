2ea5141474177af7ffb42db22630e4bd6f76103c
Nick Law <nick@nicholaslaw.com.au>
2016-05-31 07:39:44 +1000

    fixed icon and emotion build issues

--- build.js
+++ build.js
@@ -17,11 +17,12 @@ if(test('-d', sorcePath)) rm('-rf', sorcePath)
 // delete builds folder if it exists
 if(test('-d', buildPath)) rm('-rf', buildPath)
 
-// copy emojify css and emoticons
+// copy emojify css and emoticons to src
 if(!test('-f', 'src/assets/css/emojify.css'))
   cp('-f', 'node_modules/emojify.js/dist/css/basic/emojify.css', 'src/assets/css')
 if(!test('-d', 'src/assets/img/emoticons'))
   cp('-rf', 'node_modules/emojify.js/dist/images/basic', 'src/assets/img/emoticons')
+
 
 // run wepback build
 if(exec('webpack --progress --hide-modules').code !== 0) {
@@ -35,10 +36,17 @@ if(exec('webpack --progress --hide-modules').code !== 0) {
 mkdir('-p', sorcePath)
 
 // copy neccessary files and folders to source folder
-cp('-rf', [distPath, 'index.html', 'package.json', 'favicon.ico'], sorcePath)
+cp('-rf', [distPath, 'index.html', 'package.json'], sorcePath)
+
+// copy logo file source folder
+mkdir('-p', sorcePath+'/src/assets/img');
+cp('-f', 'src/assets/img/logo-100x100.png', sorcePath+'/src/assets/img');
+
+// copy emoticons source folder
+cp('-rf', 'src/assets/img/emoticons', sorcePath+'/src/assets/img/');
 
 // run builds for specified platforms
-if(exec('node node_modules/nwjs-builder/bin/nwb nwbuild -v '+nwVersion+'-sdk -p '+platforms+' --output-name "{name}-v{version}-{target}" --win-ico ./.build/favicon.ico -o '+buildPath+' '+sorcePath).code !== 0) {
+if(exec('node node_modules/nwjs-builder/bin/nwb nwbuild -v '+nwVersion+'-sdk -p '+platforms+' --output-name "{name}-v{version}-{target}" --win-ico favicon.ico --mac-icns favicon.icns -o '+buildPath+' '+sorcePath).code !== 0) {
   echo("\nFailed to complete target build process. Exiting...")
   // delete source folder
   rm('-rf', sorcePath)
@@ -48,4 +56,4 @@ if(exec('node node_modules/nwjs-builder/bin/nwb nwbuild -v '+nwVersion+'-sdk -p 
 // remove source folder
 rm('-rf', sorcePath)
 
-echo("\nBuild Process Complete!\n")
+echo("\nBuild Process Complete.\n")
