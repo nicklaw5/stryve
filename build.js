732ec4d751e516c52038ee562f19cf4dd24e75f0
Nick Law <nick@nicholaslaw.com.au>
2016-05-31 06:37:36 +1000

    adjusted build script and update package.json

--- build.js
+++ build.js
@@ -6,40 +6,46 @@ const sorcePath = '.build'
 const buildPath = 'builds'
 const platforms = 'win32,win64' // win32,win64,osx32,osx64,linux32,linux64
 
+echo("\nStarting build process. Hold on to your butt!\n")
+
 // delete dist folder if it exists
-if (test('-d', distPath)) rm('-rf', distPath)
+if(test('-d', distPath)) rm('-rf', distPath)
 
 // delete source folder if it exists
-if (test('-d', sorcePath)) rm('-rf', sorcePath)
+if(test('-d', sorcePath)) rm('-rf', sorcePath)
 
 // delete builds folder if it exists
-if (test('-d', buildPath)) rm('-rf', buildPath)
+if(test('-d', buildPath)) rm('-rf', buildPath)
 
 // copy emojify css and emoticons
 if(!test('-f', 'src/assets/css/emojify.css'))
   cp('-f', 'node_modules/emojify.js/dist/css/basic/emojify.css', 'src/assets/css')
 if(!test('-d', 'src/assets/img/emoticons'))
-  cp('-Rf', 'node_modules/emojify.js/dist/images/basic', 'src/assets/img/emoticons')
+  cp('-rf', 'node_modules/emojify.js/dist/images/basic', 'src/assets/img/emoticons')
 
 // run wepback build
 if(exec('webpack --progress --hide-modules').code !== 0) {
-  echo('Failed to execute wepack build.');
-  exit(1);
+  echo("\nFailed to complete wepack build. Exiting...")
+  // delete source folder
+  rm('-rf', sorcePath)
+  exit(1)
 }
 
 // re-create empty source folder
 mkdir('-p', sorcePath)
 
 // copy neccessary files and folders to source folder
-cp('-Rf', [distPath, 'index.html', 'package.json', 'favicon.ico'], sorcePath)
+cp('-rf', [distPath, 'index.html', 'package.json', 'favicon.ico'], sorcePath)
 
-// run builds
-if(exec('nwb nwbuild -v '+nwVersion+'-sdk -p '+platforms+' --win-ico ./favicon.ico -o '+buildPath+' '+sorcePath).code !== 0) {
-  echo('Failed to execute builds.');
-  exit(1);
+// run builds for specified platforms
+if(exec('node node_modules/nwjs-builder/bin/nwb nwbuild -v '+nwVersion+'-sdk -p '+platforms+' --output-name "{name}-v{version}-{target}" --win-ico ./.build/favicon.ico -o '+buildPath+' '+sorcePath).code !== 0) {
+  echo("\nFailed to complete target build process. Exiting...")
+  // delete source folder
+  rm('-rf', sorcePath)
+  exit(1)
 }
 
 // remove source folder
 rm('-rf', sorcePath)
 
-echo('Build Process Complete!')
+echo("\nBuild Process Complete!\n")
